e3a407e2a6c71c7679d3b560886e700b56a2d744
alecv <Alecvanspr@gmail.com>
2020-12-07 20:21:37 +0100

    Meldingen op het meldingen worden weergegeven door een foreachloopje

--- Website/Look/Models/Melding.cs
+++ Website/Look/Models/Melding.cs
@@ -1,17 +1,21 @@
-using System;
-using System.ComponentModel.DataAnnotations;
+﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace Look.Models
 {
     public class Melding
     {
-        public bool IsPrive {get; set;}
-        public DateTime AangemaaktOp {get; set;}
-        public string Titel {get; set;}
-        public int Likes {get; set;}
-        public int Views {get; set;}
-        public List<Reactie> Reacties {get; set;}
-        public List<string> Categorieen {get; set;}
+        public string meldingID {get; set;}
+        public bool IsPrive { get; set; }
+        public DateTime AangemaaktOp { get; set; }
+        public string Titel { get; set; }
+        public string Inhoud {get; set;}
+        public int Likes { get; set; }
+        public int Views { get; set; }
+        public Gebruiker auteur {get;set;}
+        public List<Reactie> Reacties { get; set; }
+        public List<string> Categorieen { get; set; }
     }
 }
