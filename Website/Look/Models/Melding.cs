bd1158d3585d8ba749be96061473d7bc809b382f
Scott van Duin <scott.vduin@gmail.com>
2020-12-09 14:12:03 +0100

    Revert "Database gecreëerd + configuratie"
    This reverts commit 485376f3438a8e3a52d01de7320000bc02874b15.

--- Website/Look/Models/Melding.cs
+++ Website/Look/Models/Melding.cs
@@ -2,21 +2,19 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using System.ComponentModel.DataAnnotations;
 
 namespace Look.Models
 {
     public class Melding
     {
-        [Key]
-        public string MeldingId {get; set;}
+        public string meldingID {get; set;}
         public bool IsPrive { get; set; }
         public DateTime AangemaaktOp { get; set; }
         public string Titel { get; set; }
         public string Inhoud {get; set;}
         public int Likes { get; set; }
         public int Views { get; set; }
-        public Gebruiker Auteur {get;set;}
+        public Gebruiker auteur {get;set;}
         public List<Reactie> Reacties { get; set; }
         public List<string> Categorieen { get; set; }
     }
