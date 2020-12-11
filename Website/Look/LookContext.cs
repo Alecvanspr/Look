d8d85d41a5e0886a42ab27f411ace5b45f7024eb
Scott van Duin <scott.vduin@gmail.com>
2020-12-11 10:09:59 +0100

    DBContext + Configuratie

---
+++ Website/Look/LookContext.cs
@@ -0,0 +1,27 @@
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Look.Models;
+
+namespace Look
+{
+    public class LookContext : DbContext
+    {
+        public DbSet<Gebruiker> Gebruikers {get; set;}
+        public DbSet<Melding> Meldingen {get; set;}
+        public DbSet<Reactie> Reacties {get; set;}
+
+        protected override void OnModelCreating(ModelBuilder ModelBuilder)
+        {
+            ModelBuilder.Entity<Gebruiker>()
+                .Property(i => i.GebruikersNummer)
+                .ValueGeneratedOnAdd();
+            ModelBuilder.Entity<Melding>()
+                .Property(i => i.MeldingId)
+                .ValueGeneratedOnAdd();
+            ModelBuilder.Entity<Reactie>()
+                .Property(i => i.ReactieId)
+                .ValueGeneratedOnAdd();    
+        }
+    }
+}
